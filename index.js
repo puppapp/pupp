bbb31a4b3bcca8a43ac62a54aafeeed775a7c460
Joao Paulo Ribeiro <jpnribeiro94@gmail.com>
2017-05-06 14:04:59 -0300

    add helmet and morgan and remove express-session

--- index.js
+++ index.js
@@ -3,24 +3,20 @@ require('./config/database')(process.env.MONGOLAB_URI || 'mongodb://localhost/pu
 
 import express from 'express'
 import path from 'path'
+import logger from 'morgan'
 import cookieParser from 'cookie-parser'
-import session from 'express-session'
 import bodyParser from 'body-parser'
+import helmet from 'helmet'
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
+app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, '/client')))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-app.use(session(
-  {
-    secret: process.env.APP_SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true
-  }
-))
+app.use(helmet())
 
 app.listen(app.get('port'), function () {
-  console.log('Server on. Running on http://localhost:3000/')
+  console.log('Server on')
 })
